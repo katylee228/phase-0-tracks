e28bebccee1a190be43fc32d3867aef5825d19d3
katylee228 <katylee228@gmail.com>
2016-12-16 15:46:45 -0800

    created nested hashes and arrays in a hash to demonstrate that I can access nested data by using index and keys

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,11 +1,28 @@
-thanksgiving = {
-  'food' => ['turkey', 'gravy', 'raspberry sauce', 'stuffing', 'mashed potato', 'pumpkin pie'],
-  'people' => ['Katy', 'Richard', 'Christine', 'Karl'],
-  'others' => ['napkins', 'cake', 'beer', 'soda', 'kambucha']
+models = {
+  "Katy Lee" => {
+    height: 170,
+    age: 25,
+    experence: true,
+    agency_name: ["LOOK", "TOP"]
+  },
+  "Sacha Badame" => {
+    height: 175,
+    age: 27,
+    experience: false,
+    agency_name: []
+  },
+  "Richard Lyons" => {
+    height: 190,
+    age:22,
+    experience: false,
+    agency_name: ["NYLO"]
+  }
 }
 
-p thanksgiving['food'][1]
-thanksgiving['food'][2] = 'cranberry sauce'
-p thanksgiving['food']
-thanksgiving['people'].push('Sean')
-p thanksgiving['people'].last
+p models["Katy Lee"][:agency_name].reverse
+p models["Sacha Badame"][:age]
+models["Richard Lyons"][:experience] = true 
+p models["Richard Lyons"]
+models["Sacha Badame"][:agency_name].push("Scout")
+p models["Sacha Badame"][:agency_name]
+p models["Katy Lee"][:agency_name][0].downcase
