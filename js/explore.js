fce6bd765cdac3f3d4ef7f9ade4f7b55b93830eb
katylee228 <katylee228@gmail.com>
2017-01-06 16:19:33 -0800

    create a function that reverse the order of letters of words

--- js/explore.js
+++ js/explore.js
@@ -1,35 +1,19 @@
-/* 
-pseudocode a function
-method: reverse a string (e.g. "hello" -> "olleh")
-input: string 
-steps:
-  #get a value for the string parameter
-  #use the length of the string to get each letter from the string
-    #set a parameter as a length of the string
-    #subtract 1 (since string index starts from 0)
-    #the parameter of the string length should be more than 0
-    #subtract 1 from the length parameter
-  #create a new string parameter with no value 
-  #add each letter of the string parameter to the new string parameter
-output: reversed string
+/*
+pseudo code for reverse function
+*create a function that reverses the order of words
+  *should take a string parameter
+  *use the length and index of the word to print from the last letter to the first letter
+    *loop should be used
+*print the reversed value
 */
 
-function reverse(str) {
-  var newstring = "";
-  for (var i = str.length -1; i >=0; i--) {
-    newstring += str[i];
+function reverse(string) {
+  var new_str = ""
+  for (var i = string.length -1; i >= 0; i --) {
+    new_str += string[i];
   }
-  
-  return newstring;
+  return new_str;
 }
 
-console.log(reverse("morning!"));
-
-var variable = reverse("morning!");
-var feelgood = true;
-var morning = true;
-
-if (feelgood && morning) {
-  console.log(variable);
-}
+console.log(reverse("hello!"))
 
