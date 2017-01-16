19e5645555db5992e0f79a2b1b173fabb53d40a1
Hwaeun Lee <Katylee@Hwaeuns-MacBook.local>
2017-01-15 21:34:08 -0800

    create a method that creates data in the table

--- databases/sql_ruby/find_match.rb
+++ databases/sql_ruby/find_match.rb
@@ -7,3 +7,9 @@ create_table_cmd = <<-SQL
 CREATE TABLE IF NOT EXISTS match_info(id INTEGER PRIMARY KEY, prefix VARCHAR(255), name VARCHAR(255), age INT, location VARCHAR(255))
 SQL
 
+db.execute(create_table_cmd)
+
+def create_match(db, prefix,name,age,location)
+	db.execute("INSERT INTO match_info(prefix,name,age,location) VALUES(?,?)", [prefix,name,age,location])
+end
+
