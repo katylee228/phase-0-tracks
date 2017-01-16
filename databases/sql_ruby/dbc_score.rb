22dd847460bd9e10d6b908189f974e38bd4353e6
Hwaeun Lee <Katylee@Hwaeuns-MacBook.local>
2017-01-15 23:19:23 -0800

    added the email feature that sends emails to students who passed the test

--- databases/sql_ruby/dbc_score.rb
+++ databases/sql_ruby/dbc_score.rb
@@ -1,6 +1,8 @@
 require 'sqlite3'
 require 'faker'
-
+require 'net/smtp'
+require 'rubygems'
+require 'mailfactory'
 
 db = SQLite3::Database.new("scores.db")
 db.results_as_hash = true
@@ -27,5 +29,19 @@ puts "Sorting students who passed the test:"
 scores.each do |student|
 	if student['scores'] > 60 
 		puts "#{student["id"]}|#{student['name']}"
+=begin
+		mail = MailFactory.new()
+  		mail.to = "#{student['email']}"
+  		mail.from = "sender@sender.com"
+  		mail.subject = "Great news from DBC!"
+  		mail.text = "Hello #{student['name']}, 
+  					Congratuations! You passed the test!"
+
+  Net::SMTP.start('smtp1.testmailer.com', 25, 'mail.from.domain') { |smtp|
+    mail.to = toaddress
+    smtp.send_message(mail.to_s(), toaddress)
+  }
+=end
 	end 
 end 
+
