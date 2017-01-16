021f737ca9775c82f02d7e9c953c6eedfb20f9e3
Hwaeun Lee <Katylee@Hwaeuns-MacBook.local>
2017-01-15 23:04:58 -0800

    using each method, I sorted students who scored more than 60/100

--- databases/sql_ruby/dbc_score.rb
+++ databases/sql_ruby/dbc_score.rb
@@ -1,11 +1,31 @@
 require 'sqlite3'
 require 'faker'
 
+
 db = SQLite3::Database.new("scores.db")
-db.results_as_hash = true 
+db.results_as_hash = true
 
 create_table_cmd = <<-SQL
-CREATE TABLE IF NOT EXISTS scores(id INTEGER PRIMARY KEY, name VARCHAR(255), scores INT)
+CREATE TABLE IF NOT EXISTS scores(id INTEGER PRIMARY KEY, name VARCHAR(255), scores INT, email VARCHAR(355))
 SQL
 
 db.execute(create_table_cmd)
+
+
+def create_scores(db, name, scores,email)
+	db.execute("INSERT INTO scores(name, scores,email) VALUES(?,?,?)", [name,scores,email])
+end
+
+100.times do 
+	create_scores(db, Faker::Name.name, Faker::Number.between(0,100), Faker::Internet.email)
+end
+
+scores = db.execute("SELECT * FROM scores")
+
+
+puts "Sorting students who passed the test:"
+scores.each do |student|
+	if student['scores'] > 60 
+		puts "#{student["id"]}|#{student['name']}"
+	end 
+end 
