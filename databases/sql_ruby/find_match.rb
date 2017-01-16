a21db2e8e87359773e6d0a9c5bfc39e5c879eb9c
Hwaeun Lee <Katylee@Hwaeuns-MacBook.local>
2017-01-15 21:28:10 -0800

    added create table command

--- databases/sql_ruby/find_match.rb
+++ databases/sql_ruby/find_match.rb
@@ -2,3 +2,8 @@ require 'sqlite3'
 require 'faker'
 
 db = SQLite3::Database.new("match_info.db")
+
+create_table_cmd = <<-SQL 
+CREATE TABLE IF NOT EXISTS match_info(id INTEGER PRIMARY KEY, prefix VARCHAR(255), name VARCHAR(255), age INT, location VARCHAR(255))
+SQL
+
